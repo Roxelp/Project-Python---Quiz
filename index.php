c28a06e465cd3dba6492c96f9e0b2c726a158ddf
Roxelp <roxelpdesign@gmail.com>
2017-01-27 13:24:13 +0300

    Dodano wyswietlanie testw w index.php i skrypt dodajacy testy

--- index.php
+++ index.php
@@ -2,11 +2,18 @@
 <!DOCTYPE html>
 <html>
   <head>
+      <style>
+          table, tr, td {
+              border: 1px solid #000;
+              border-collapse: collapse;
+          }
+      </style>
     <meta charset="UTF-8">
     <title>title</title>
   <link rel="stylesheet" href="stylesheet.css" type="text/css">
   </head>
   <body>
+      <div style="width: 50%; float:left">
   <form method="POST">
     <input type="number" name="id_testu">   <br>
       <input type="text" name="pytanie">   <br>
@@ -16,10 +23,7 @@
       <input type="text" name="odp4">   <br>
       <input type="number" name="nr_odp"> <br>
       <input type="submit">
-  </form>
-  </body>
-</html>
-<?php 
+      <?php 
 
 session_start();
 
@@ -32,11 +36,6 @@ if(isset($_POST["id_testu"])) {
     $odp3 = $_POST["odp3"];
     $odp4 = $_POST["odp4"];
     $prawidlowa = $_POST["nr_odp"];
-    
-    $host = "localhost";
-    $db_user = "root";
-    $db_password = "root";
-    $db_name = "test";
     
     if(empty($id) || empty($pytanie) || empty($odp1)|| empty($odp2) || empty($odp3)|| empty($odp4)|| empty($prawidlowa))    {
         echo "Uzupełnij wszystkie pola!";
@@ -73,3 +72,53 @@ if(isset($_POST["id_testu"])) {
 }
 
 ?>
+  </form>
+      </div>
+      <div style="width: 50%; float:left">
+        <table>
+        <?php 
+          
+    $host = "localhost";
+    $db_user = "root";
+    $db_password = "root";
+    $db_name = "test";
+    
+            try 
+            {
+                $polaczenie = new mysqli($host, $db_user, $db_password, $db_name);
+                if ($polaczenie->connect_errno!=0)
+                {
+                    throw new Exception(mysqli_connect_errno());
+                }
+                else
+                    {
+                        $sql = "SELECT * FROM testy";
+                        $rezultat = $polaczenie->query($sql);
+                    
+                        if($rezultat->num_rows > 0) {
+                            while($row = $rezultat->fetch_assoc()) {
+                                echo "
+                                    <tr>
+                                        <td>". $row['id_testu']  ."</td>
+                                        <td>". $row['nazwa'] ."</td>
+                                        <td>". $row['kategoria']. "</td>
+                                        <td>". $row['trudnosc'] ."</td>
+                                    </tr>
+                                ";
+                            }
+                        }
+                }
+
+                    $polaczenie->close();
+                }
+            catch(Exception $e)
+            {
+                echo '<span style="color:red;">Błąd serwera! Przepraszamy za niedogodności i prosimy o rejestrację w innym terminie!</span>';
+                echo '<br />Informacja developerska: '.$e;
+            }  
+          
+        ?>
+        </table>
+      </div>
+  </body>
+</html>
